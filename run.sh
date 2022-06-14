537d5b55bbc6ca91da78d6e4677224949a2397cf
Stella Hansen <458537@ac02zc4tzlvdq.genpt.net>
2022-06-14 11:47:33 -0400

    'I want my comment to be this'

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ version=$(echo $first | egrep -o '[0-9a-z]{40}')
 #version=$(echo "$IN" | cut -d ":" -f 2)
 #comment=
 filename="$(echo "$filename" | sed -e "s/^'//" -e "s/'$//")"
+echo $filename
 #filename="test"
 git checkout "$version" -- "$filename"
 git add .
