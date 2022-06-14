92ebc944415c3e585945348f5cab250e664173d4
Stella Hansen <458537@ac02zc4tzlvdq.genpt.net>
2022-06-14 11:24:10 -0400

    a

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-#version="$(cat /Users/458537/Desktop/file.sh)"
-first="$(grep -i "version:" file.sh)"
+#version="$(cat )"
+first="$(grep -i "version:" /Users/458537/Desktop/file.sh)"
 version=$(echo $first | egrep -o '[0-9a-z]{40}')
 #IN="$(grep -i -E "version:[0..9a..z]\{5,44}" /Users/458537/Desktop/file.sh)"
+#version=$(echo "$IN" | cut -d ":" -f 2)
 filename="test"
 git checkout "$version" -- "$filename"
 git add .
