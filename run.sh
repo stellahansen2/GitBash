258778d83e02fd998c8fcac64216f6d6f22483b1
Stella Hansen <458537@ac02zc4tzlvdq.genpt.net>
2022-06-14 11:21:22 -0400

    too many changes

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #version="$(cat /Users/458537/Desktop/file.sh)"
-IN="$(grep -i "version" /Users/458537/Desktop/file.sh)"
-version=$(echo "$IN" | cut -d ":" -f 2)
+first="$(grep -i "version:" file.sh)"
+version=$(echo $first | egrep -o '[0-9a-z]{40}')
+#IN="$(grep -i -E "version:[0..9a..z]\{5,44}" /Users/458537/Desktop/file.sh)"
 filename="test"
 git checkout "$version" -- "$filename"
 git add .
