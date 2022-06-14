719e3bf4f4db236441f2c9dec2cce1db472b337d
Stella Hansen <458537@ac02zc4tzlvdq.genpt.net>
2022-06-14 11:50:24 -0400

    I want my comment to be this

--- run.sh
+++ run.sh
@@ -5,12 +5,13 @@ third="$(grep -i "filename:" /Users/458537/Desktop/file.sh)"
 version=$(echo $first | egrep -o '[0-9a-z]{40}')
 #IN="$(grep -i -E "version:[0..9a..z]\{5,44}" /Users/458537/Desktop/file.sh)"
 #version=$(echo "$IN" | cut -d ":" -f 2)
-#comment=
+comment="$(echo $second | egrep -o "'.*'")"
+updateComment="$(echo "$comment" | sed -e "s/^'//" -e "s/'$//")"
 filename1="$(echo $third | egrep -o "'.*'")"
 filename="$(echo "$filename1" | sed -e "s/^'//" -e "s/'$//")"
 echo $filename
 #filename="test"
 git checkout "$version" -- "$filename"
 git add .
-git commit -m "$(echo $second | egrep -o "'.*'")"
+git commit -m "$updateComment"
 git push
