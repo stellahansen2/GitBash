d4ae445cb7555e86be01c32c5d551f553a9b5872
Stella Hansen <458537@ac02zc4tzlvdq.genpt.net>
2022-06-14 11:40:12 -0400

    a

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ version=$(echo $first | egrep -o '[0-9a-z]{40}')
 #IN="$(grep -i -E "version:[0..9a..z]\{5,44}" /Users/458537/Desktop/file.sh)"
 #version=$(echo "$IN" | cut -d ":" -f 2)
 #comment=
-#filename=
-git checkout "$version" -- "$(echo $third | egrep -o "'.*'")"
+filename=$(echo "$filename" | sed -e "s/^'//" -e "s/'$//")
+git checkout "$version" -- "$filename"
 git add .
 git commit -m "$(echo $second | egrep -o "'.*'")"
 git push
