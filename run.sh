f5b35d79fbf69f97bfb0ead46c183235cf34a5e0
Stella Hansen <458537@ac02zc4tzlvdq.genpt.net>
2022-06-14 11:48:58 -0400

    'I want my comment to be this'

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ version=$(echo $first | egrep -o '[0-9a-z]{40}')
 #IN="$(grep -i -E "version:[0..9a..z]\{5,44}" /Users/458537/Desktop/file.sh)"
 #version=$(echo "$IN" | cut -d ":" -f 2)
 #comment=
-filename="$(echo "$filename" | sed -e "s/^'//" -e "s/'$//")"
+filename1="$(echo $third | egrep -o "'.*'")"
+filename="$(echo "$filename1" | sed -e "s/^'//" -e "s/'$//")"
 echo $filename
 #filename="test"
 git checkout "$version" -- "$filename"
